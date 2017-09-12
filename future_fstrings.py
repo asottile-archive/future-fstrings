d9dc9ff961ddf9360b1219d922900b73c3154cfc
Anthony Sottile <asottile@umich.edu>
2017-09-11 23:30:50 -0700

    Rewrite string parser from py36 implementation

--- future_fstrings.py
+++ future_fstrings.py
@@ -1,32 +1,144 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import codecs
-import string
 
 encode = codecs.utf_8_encode
 
-_parse_format = string.Formatter().parse
+
+def _find_literal(s, start, level, parts, exprs):
+    """Roughly Python.ast.c:fstring_find_literal"""
+    i = start
+    parse_expr = True
+
+    while i < len(s):
+        ch = s[i]
+
+        if ch in ('{', '}'):
+            if level == 0:
+                if i + 1 < len(s) and s[i + 1] == ch:
+                    i += 2
+                    parse_expr = False
+                    break
+                elif ch == '}':
+                    raise AssertionError("f-string: single '}' is not allowed")
+            break
+
+        i += 1
+
+    parts.append(s[start:i])
+    return i, parse_expr and i < len(s)
+
+
+def _find_expr(s, start, level, parts, exprs):
+    """Roughly Python.ast.c:fstring_find_expr"""
+    i = start
+    nested_depth = 0
+    quote_char = None
+    triple_quoted = None
+
+    def _check_end():
+        if i == len(s):
+            raise AssertionError("f-string: expecting '}'")
+
+    if level >= 2:
+        raise AssertionError("f-string: expressions nested too deeply")
+
+    parts.append(s[i])
+    i += 1
+
+    while i < len(s):
+        ch = s[i]
+
+        if ch == '\\':
+            raise AssertionError(
+                'f-string expression part cannot include a backslash',
+            )
+        if quote_char is not None:
+            if ch == quote_char:
+                if triple_quoted:
+                    if i + 2 < len(s) and s[i + 1] == ch and s[i + 2] == ch:
+                        i += 2
+                        quote_char = None
+                        triple_quoted = None
+                else:
+                    quote_char = None
+                    triple_quoted = None
+        elif ch in ('"', '"'):
+            quote_char = ch
+            if i + 2 < len(s) and s[i + 1] == ch and s[i + 2] == ch:
+                triple_quoted = True
+                i += 2
+            else:
+                triple_quoted = False
+        elif ch in ('[', '{', '('):
+            nested_depth += 1
+        elif nested_depth and ch in (']', '}', ')'):
+            nested_depth -= 1
+        elif ch == '#':
+            raise AssertionError("f-string expression cannot include '#'")
+        elif nested_depth == 0 and ch in ('!', ':', '}'):
+            if ch == '!' and i + 1 < len(s) and s[i + 1] == '=':
+                # Allow != at top level as `=` isn't a valid conversion
+                pass
+            else:
+                break
+        i += 1
+
+    if quote_char is not None:
+        raise AssertionError('f-string: unterminated string')
+    elif nested_depth:
+        raise AssertionError("f-string: mismatched '(', '{', or '['")
+    _check_end()
+
+    exprs.append(s[start + 1:i])
+
+    if s[i] == '!':
+        parts.append(s[i])
+        i += 1
+        _check_end()
+        parts.append(s[i])
+        i += 1
+
+    _check_end()
+
+    if s[i] == ':':
+        parts.append(s[i])
+        i += 1
+        _check_end()
+        i = _fstring_parse(s, i, level + 1, parts, exprs)
+
+    _check_end()
+    if s[i] != '}':
+        raise AssertionError("f-string: expecting '}'")
+
+    parts.append(s[i])
+    i += 1
+    return i
+
+
+def _fstring_parse(s, i, level, parts, exprs):
+    """Roughly Python.ast.c:fstring_find_literal_and_expr"""
+    while i < len(s):
+        i, parse_expr = _find_literal(s, i, level, parts, exprs)
+        if i == len(s) or s[i] == '}':
+            return i
+        if parse_expr:
+            i = _find_expr(s, i, level, parts, exprs)
+    return i
+
+
+def _fstring_reformat(s):
+    parts, exprs = [], []
+    _fstring_parse(s, i=0, level=0, parts=parts, exprs=exprs)
+    exprs = ['({})'.format(expr) for expr in exprs]
+    return '{}.format({})'.format(''.join(parts), ', '.join(exprs))
 
 
 def _make_fstring(src):
     import tokenize_rt
 
-    parts = []
-
-    def _convert_tup(tup):
-        ret, field_name, format_spec, conversion = tup
-        ret = ret.replace('{', '{{').replace('}', '}}')
-        if field_name is not None:
-            parts.append(field_name)
-            ret += '{'
-            if conversion:
-                ret += '!' + conversion
-            if format_spec:
-                ret += ':' + format_spec
-            ret += '}'
-        return ret
-
-    new_str = ''.join(_convert_tup(tup) for tup in _parse_format(src))
-    new_src = '{}.format({})'.format(new_str, ', '.join(parts))
-    return [tokenize_rt.Token(name='FSTRING', src=new_src)]
+    return [tokenize_rt.Token(name='FSTRING', src=_fstring_reformat(src))]
 
 
 def decode(b, errors='strict'):
--- tests/future_fstrings_test.py
+++ tests/future_fstrings_test.py
@@ -1,4 +1,9 @@
 # -*- coding: future_fstrings -*-
+import pytest
+
+import future_fstrings
+
+
 def test_hello_world():
     thing = 'world'
     assert f'hello {thing}' == 'hello world'
@@ -6,3 +11,75 @@ def test_hello_world():
 
 def test_maths():
     assert f'{5 + 5}' == '10'
+
+
+def test_with_bangs():
+    thing = 'world'
+    assert f'hello {thing.replace("d", "d!")}' == 'hello world!'
+
+
+def test_with_braces():
+    assert f'hello {{ hi }}' == 'hello { hi }'
+
+
+def test_strings_quoting_variables():
+    assert f'hello {"{x}"}' == 'hello {x}'
+    assert f"hello {'{x}'}" == 'hello {x}'
+    assert f'hello {"""{x}"""}' == 'hello {x}'
+    assert f"hello {'''{x}'''}" == 'hello {x}'
+
+
+def test_sequence_literals():
+    assert f'hello {[1, 2, 3][0]}' == 'hello 1'
+    assert f'hello {sorted({1, 2, 3})[0]}' == 'hello 1'
+    assert f'hello {(1, 2, 3)[0]}' == 'hello 1'
+
+
+def test_nested_format_literals():
+    x = 5
+    y = 6
+    assert f'{x:{y}d}' == '     5'
+
+
+def _assert_fails_with_msg(s, expected_msg):
+    with pytest.raises(AssertionError) as excinfo:
+        future_fstrings._fstring_reformat(s)
+    msg, = excinfo.value.args
+    assert msg == expected_msg
+
+
+def test_error_only_one_closing_paren():
+    _assert_fails_with_msg("'hi }'", "f-string: single '}' is not allowed")
+
+
+def test_error_unterminated_brace():
+    _assert_fails_with_msg("'hi {'", "f-string: expecting '}'")
+
+
+def test_error_backslash():
+    _assert_fails_with_msg(
+        r"""'hi {"\\"}'""",
+        'f-string expression part cannot include a backslash',
+    )
+
+
+def test_error_contains_comment_character():
+    _assert_fails_with_msg(
+        "'hi {#}'", "f-string expression cannot include '#'",
+    )
+
+
+def test_unterminated_quotes():
+    _assert_fails_with_msg("""'hi {"s""", 'f-string: unterminated string')
+
+
+def test_incorrectly_nested_braces():
+    _assert_fails_with_msg(
+        "'hi {[1, 2'", "f-string: mismatched '(', '{', or '['",
+    )
+
+
+def test_too_deep():
+    _assert_fails_with_msg(
+        "'{x:{y:{z}}}'", 'f-string: expressions nested too deeply',
+    )
